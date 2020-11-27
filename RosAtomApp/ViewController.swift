e8c3c0240bd3b34a5c5b9331ac8501cdeaac53f6
Admin <>
2020-11-27 22:31:01 +0300

    add shadows

--- RosAtomApp/ViewController.swift
+++ RosAtomApp/ViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     
     @IBOutlet weak var numberField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
+    
     @IBOutlet weak var enterButtonOutlet: UIButton!
     @IBAction func enterButton(_ sender: UIButton) {
+        
     }
     
     @IBOutlet weak var numberView: UIView!
@@ -24,9 +26,34 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupSubviews()
     }
-
-
+    
+    private func setupSubviews() {
+        view.backgroundColor = .white
+        enterButtonOutlet.layer.cornerRadius = 13
+        numberView.layer.cornerRadius = 15
+        passwordView.layer.cornerRadius = 15
+        setShadow(view: numberView)
+        setShadow(view: passwordView)
+        numberField.keyboardType = .numberPad
+        passwordField.isSecureTextEntry = true
+    }
+    
+    func setShadow(view: UIView) {
+        view.layer.shadowColor = UIColor(red: 0.488, green: 0.785, blue: 1, alpha: 0.25).cgColor
+        view.layer.shadowOpacity = 1
+        view.layer.shadowOffset = .zero
+        view.layer.shadowRadius = 15
+        view.layer.shadowPath = UIBezierPath(roundedRect: view.bounds, cornerRadius: 15).cgPath
+        view.layer.shouldRasterize = true
+        view.layer.rasterizationScale = UIScreen.main.scale
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    
 }
 
