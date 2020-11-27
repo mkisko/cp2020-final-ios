b10b1cce2b7a254ff97c7ebbe398a59d741a25b6
Admin <>
2020-11-27 18:20:53 +0300

    initial commit

---
+++ RosAtomApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RosAtomApp
+//
+//  Created by Admin on 27.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
